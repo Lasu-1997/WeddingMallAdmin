689eb81bf52110f1ae5909d32121e01cc94b3ec1
Shehan Udaraka <shehantharuprocessing@gmail.com>
2023-05-10 17:23:01 +0530

    Added service controller and view.

---
+++ WeddingMallAdmin/Controllers/ServiceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WeddingMallAdmin.Models;
+
+namespace WeddingMallAdmin.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Service> services = new List<Service>();
+            using(var client = new HttpClient())
+            {
+                using(var response = await client.GetAsync("https://localhost:7268/api/Service/Getall"))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+                    services = JsonConvert.DeserializeObject<List<Service>>(apiResponse);
+                }
+            }
+            return View(services);
+        }
+    }
+}
